11e54af5da5c48897d12414f2f51dbc85d02a09a
Aleviannaf <alexandrevianna511@gmail.com>
2022-03-31 11:50:55 -0400

    pega opçao do jogador

---
+++ src/js/verificar-jogada.js
@@ -0,0 +1,15 @@
+const teste = document.getElementById('jogo');
+
+const cores = ['red'];
+
+function opcaoJogada(event){
+    const ok = event.target;
+    console.log(ok);
+
+    
+    
+}
+
+
+
+teste.addEventListener('click', opcaoJogada);
