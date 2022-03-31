60b14c0e1fce40591d564a5664b3bbd640074463
RodolfoJaques <rodolfojaques13@gmail.com>
2022-03-31 12:38:27 -0300

    Criaçao do acender dos botoes

--- src/js/script.js
+++ src/js/script.js
@@ -5,15 +5,23 @@ function criaTabuleiroDoJogo(){
 
     const boxRed = document.createElement('div');
     boxRed.setAttribute('class','box red');
+    boxRed.setAttribute('id','');
+
 
     const boxBlue = document.createElement('div');
     boxBlue.setAttribute('class', 'box blue');
+    boxBlue.setAttribute('id','');
+
 
     const boxGreen = document.createElement('div');
     boxGreen.setAttribute('class', 'box green');
+    boxGreen.setAttribute('id','');
+
 
     const boxYellow = document.createElement('div');
     boxYellow.setAttribute('class', 'box yellow');
+    boxYellow.setAttribute('id','');
+
 
     const boxInfos = document.createElement('div');
     boxInfos.setAttribute('class', 'box infos');
@@ -32,3 +40,31 @@ function corRandomica(){
     
     return Math.trunc(Math.random() * 4) + 1;
 }
+
+// Função que assosia numero a cor.
+function associandoNumACores(num){
+
+    const boxRed = document.querySelector('.red');
+    const boxBlue = document.querySelector('.blue');
+    const boxGreen = document.querySelector('.green');
+    const boxYellow = document.querySelector('.yellow');
+
+    if(num === 1){
+        boxRed.id = 'red';
+    }else if(num === 2){
+        boxBlue.id = 'blue';
+    }else if(num === 3){
+        boxGreen.id = 'green';
+    }else if(num === 4){
+        boxYellow.id = 'yellow';
+    }
+
+    setTimeout(function(){
+        boxRed.id = '';
+        boxBlue.id = '';
+        boxGreen.id = '';
+        boxYellow.id = '';
+    },1000);
+    
+}
+associandoNumACores(corRandomica());
